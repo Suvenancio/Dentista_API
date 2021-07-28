c4bc296c8d6a570930f4af0ad6510994593d4852
Suvenancio <venancisuh@gmail.com>
2021-07-27 23:20:08 -0300

    criando rotas CRUD e aplicando métodos criados na DentistaDAO

---
+++ src/controllers/dentistacontroler.js
@@ -0,0 +1,64 @@
+const DentistaDAO = require('../DAO/dentistaDAO')
+const Dentista = require('../model/dentistamodel')
+
+module.exports = (app,bd) =>{
+    const DaoDentista = new DentistaDAO(bd)
+
+    app.get('/dentista', async (req, res)=>{
+        try{
+            const verDentista = await DaoDentista.MostrarDentista()
+                res.status(200).json(verDentista)
+        }catch(e){
+            res.status(500).json(e)
+        }
+    })
+
+    app.get('/dentista/:ID', async (req,res)=>{
+        
+        try{
+            const id = req.params.ID;
+            const verUmDentista = await DaoDentista.MostrarUmDentista(id)
+            res.status(200).json(verUmDentista)
+            }catch(e){
+                res.status(500).json(e)
+            }
+    })
+
+    app.post('/dentista', async (req,res)=>{
+        const {NOME, EMAIL, CRO, ESPECIALIDADE} = req.body
+        const newDentista = new Dentista(NOME, EMAIL, CRO, ESPECIALIDADE);
+
+        try{
+            const inserirDentista = await DaoDentista.NovoDentista(newDentista)
+            res.status(200).json(inserirDentista)
+        }catch(e){
+            res.status(500).json(e)
+        }
+    })
+
+    app.delete('/dentista/:CRO', async (req,res)=>{
+        const conselho = req.params.CRO
+        
+        try{
+           const deletarDentista = await DaoDentista.DeletarDentista(conselho)
+           res.status(200).json(deletarDentista)
+        }catch(e){
+            res.status(500).json(e)
+        }
+    })
+
+    app.put('/dentista/:CRO', async (req,res)=>{
+        const conselho = req.params.CRO
+        const body = req.body
+        const infos = [body.NOME, body.EMAIL, body.CRO, body.ESPECIALIDADE]
+    
+
+        try{
+            const alterarDentista = await DaoDentista.AlterarDentista(infos,conselho)
+            res.status(200).json(alterarDentista)
+        }catch(e){
+            res.status(500).json(e)
+        }
+
+    })
+}
