384570419b99d06b29d6279f6e5ce0a6928b3caf
Suvenancio <venancisuh@gmail.com>
2021-07-27 23:13:14 -0300

    criando model e determinando atributos da tabela

---
+++ src/model/dentistamodel.js
@@ -0,0 +1,9 @@
+module.exports = class Dentista {
+    constructor(nome, email, CRO, especilidade){
+        this.nome = nome
+        this.email = email
+        this.CRO = CRO
+        this.especilidade = especilidade
+
+    }
+}
