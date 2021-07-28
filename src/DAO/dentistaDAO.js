7db34fb946217941edfddebffd25a12f7c66dcf1
Suvenancio <venancisuh@gmail.com>
2021-07-27 23:17:34 -0300

    adicionando pasta DAO e criando métodos a classe DentistaDAO para aplicação do modelo CRUD

---
+++ src/DAO/dentistaDAO.js
@@ -0,0 +1,66 @@
+module.exports = class DentistaDAO {
+
+    constructor(bd){
+        this.bd = bd
+    }
+
+    MostrarDentista(){
+        return new Promise((resolve,reject)=>{
+            const query = 'SELECT * FROM DENTISTA'
+
+            this.bd.all(query, (e,res)=>{
+                if(e) reject(`Erro ao mostrar o BD ${e}`)
+                else resolve(res)
+            })
+        })
+    }
+    MostrarUmDentista(id){
+        return new Promise((resolve, reject)=>{
+            const query ='SELECT * FROM DENTISTA WHERE ID = (?)'
+            this.bd.all(query,id,(e,res)=>{
+                if(e) reject('Erro ao acessar BD')
+                else resolve(res)
+            })
+        })
+    }
+    NovoDentista(newDentista){
+        return new Promise((resolve, reject)=>{
+            const query ='INSERT INTO DENTISTA (NOME, EMAIL, CRO, ESPECIALIDADE) VALUES (?,?,?,?)'
+
+            this.bd.run(query,Object.values(newDentista), (e)=>{
+                if(e) reject(`Erro ao adicionar BD ${e}`)
+                else resolve('Dentista adicionado com sucesso')
+            })
+        })
+    }
+
+    DeletarDentista(newDentista) {
+        return new Promise ((resolve, reject) => {
+            const query = 'DELETE FROM DENTISTA WHERE CRO = (?)'
+
+            this.bd.run(
+                query, newDentista,
+                (e,res) => {
+                    if(e) reject(`Erro ao deletar Dentista ${e}`)
+                else resolve('Dentista excluido com sucesso')
+                
+            })
+        })
+    }
+
+    AlterarDentista(infos,CRO){
+            
+        return new Promise((resolve,reject)=>{
+
+            const query ='UPDATE DENTISTA SET NOME = (?), EMAIL = (?), CRO = (?), ESPECIALIDADE = (?) WHERE CRO = (?)'
+            const parametros = [infos[0],infos[1],infos[2],infos[3],CRO]
+            console.log(infos)
+                
+            this.bd.run(query, parametros, (e,res)=>{
+                if(e) reject(`Erro ao editar BD ${e}`)
+                else resolve('Dentista alterado com sucesso')
+            })
+         })
+    }
+
+}
